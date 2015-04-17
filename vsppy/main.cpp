b43b2440be7457adbdfb23390fc8aafbf1d8b1ae
hirokib <ebutterfield16@cmc.edu>
2015-04-16 22:32:33 -0700

    changed fstream to use boost

--- vsppy/main.cpp
+++ vsppy/main.cpp
@@ -12,6 +12,7 @@
 #include <boost\date_time\posix_time\posix_time.hpp>
 #include <algorithm>
 #include <boost\filesystem.hpp>
+#include <boost\filesystem\fstream.hpp>
 
 #include "quote.h"
 #include "stockticks.h"
@@ -31,11 +32,13 @@ int main(int argc, char** argv) {
 	fs::path data_dir("./data");
 	std::cout << data_dir << std::endl;
 
+
+	std::vector<fs::path> vec;
+
 	try {
 		if (fs::is_directory(data_dir)){
 			std::cout << "is a directory to data" << std::endl;
 
-			std::vector<fs::path> vec;
 			std::copy(fs::directory_iterator(data_dir), fs::directory_iterator(), std::back_inserter(vec));
 			sort(vec.begin(), vec.end());
 
@@ -43,8 +46,15 @@ int main(int argc, char** argv) {
 
 			fs::directory_iterator end;
 			for (fs::directory_iterator iter(data_dir); iter != end; ++iter) {
-				cout << fs::is_regular_file(*iter) << endl;
+				//vec.push_back(iter->path());
+				//cout << iter->path().filename() << endl;
+
 			}
+
+			for (std::vector<fs::path>::iterator iter = (vec.begin()); iter != vec.end(); ++iter) {
+				cout << *iter << endl;
+			}
+
 
 
 		}
@@ -57,10 +67,10 @@ int main(int argc, char** argv) {
 	}
 
 
+	cout << 123 << endl;
 
 
-
-	std::ifstream file("data\\GOOG.csv");
+	fs::ifstream file(vec[0]);
 
 	if (file.is_open()) {
 		std::cout << "file is open" << std::endl;
@@ -80,11 +90,14 @@ int main(int argc, char** argv) {
 	while (file.good()) {
 		read(file, st);
 	}
-	std::cout << st.stocks.size() << std::endl;
+	
+	
+	
+	//std::cout << st.stocks.size() << std::endl;
 
-	for (auto ix = st.stocks.begin(); ix != st.stocks.end() && !st.stocks.empty(); ++ix) {
-		// (*ix).printDate();
-	}
+	//for (auto ix = st.stocks.begin(); ix != st.stocks.end() && !st.stocks.empty(); ++ix) {
+	//	// (*ix).printDate();
+	//}
 
 
 	file.close();
